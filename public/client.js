ed8a29136d75a2f40661d196def42de950052a16
Masho Ado <mashoado@gmail.com>
2021-09-19 00:19:48 +0300

    remove menu when nav link is clicked

--- public/client.js
+++ public/client.js
@@ -1,10 +1,20 @@
 
 const menuBtn = document.getElementsByClassName("menu-btn")[0]
+const navLinks = document.getElementsByClassName("nav-link")
 
-console.log(menuBtn)
+const navLinksarr = [...navLinks]
+console.log(navLinksarr)
+
 const menu = document.getElementsByClassName("menu-items")[0]
 
 // Add or remove menu on clicking menu button
 menuBtn.addEventListener("click", () =>{
     menu.classList.toggle("active")
+})
+
+// remove menu when nav link is clicked
+navLinksarr.forEach(link =>{
+    link.addEventListener("click", () =>{
+    menu.classList.remove("active")
+    })
 })
