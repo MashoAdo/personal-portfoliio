4118285c4cd75b4a17731d8241bbf12539038f3c
Masho Ado <mashoado@gmail.com>
2022-02-16 10:35:39 +0300

    remove google ouath2 tokenization and verification

--- server.js
+++ server.js
@@ -22,36 +22,6 @@ app.get("/", (req, res) => {
 	res.render("index.ejs");
 });
 
-// Creating Oauth2 to enable sending the email safely
-const OAuth2 = google.auth.OAuth2;
-
-// setup auth2Client
-const OAuth2Client = new OAuth2(
-	process.env.CLIENT_ID,
-	process.env.CLIENT_SECRET
-	// "https://developers.google.com/oauthplayground"
-);
-// // get access token using refresh token
-OAuth2Client.setCredentials({
-	refresh_token: process.env.REFRESH_TOKEN,
-});
-
-const accessToken = OAuth2Client.getAccessToken();
-// create nodemailer transporter
-let transporter = nodemailer.createTransport({
-	service: "gmail",
-	auth: {
-		type: "OAuth2",
-		user: process.env.EMAIL_USER,
-		client_id: process.env.CLIENT_ID,
-		client_secret: process.env.CLIENT_SECRET,
-		refresh_token: process.env.REFRESH_TOKEN,
-		accessToken: process.env.ACCESS_TOKEN || accessToken,
-	},
-	tls: {
-		rejectUnauthorized: false,
-	},
-});
 
 // post request to receive data from the form and send information to my email
 app.post("/email", (req, res) => {
