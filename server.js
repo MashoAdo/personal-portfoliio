e2ee41887bad09c3ce1975453bebc631ca7dc1df
mashoado@gmail.com <masho ado>
2022-06-07 16:04:28 +0300

    version 1.2

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 const app = express();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 // middlewares
 app.use(compression());
@@ -20,29 +20,29 @@ app.set("view engine", "ejs");
 
 // render home page
 app.get("/", (req, res) => {
-	res.render("index.ejs");
+  res.render("index.ejs");
 });
 
 // post request to receive data from the form and send information to my email
 app.post("/email", (req, res) => {
-	// get input from the form and use the info in the mailOptions of nodemailer
-	const { email, message } = req.body;
+  // get input from the form and use the info in the mailOptions of nodemailer
+  const { email, message } = req.body;
 
-	// configure mail options with form data
-	const mailOptions = {
-		from: `mashoado@gmail.com`,
-		to: "mashoado@gmail.com",
-		subject: "Employers from portfolio",
-		text: `${(message, email)}`,
-	};
+  // configure mail options with form data
+  const mailOptions = {
+    from: `mashoado@gmail.com`,
+    to: "mashoado@gmail.com",
+    subject: "Employers from portfolio",
+    text: `${(message, email)}`,
+  };
 
-	transporter.sendMail(mailOptions, (error, info) => {
-		// use ternary operator
-		error ? console.log(error) : transporter.close();
-	});
-	res.send("success");
+  transporter.sendMail(mailOptions, (error, info) => {
+    // use ternary operator
+    error ? console.log(error) : transporter.close();
+  });
+  res.send("success");
 });
 
 app.listen(PORT, () => {
-	console.log(`Listening on port 8000`);
+  console.log(`Listening on port 8000`);
 });
