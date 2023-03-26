50c0246a6aff27e4ccb99c52925741f89a0aa7c1
Masho Ado <mashoado@gmail.com>
2023-03-26 17:49:22 +0300

    remove log statements

--- server.js
+++ server.js
@@ -14,8 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/public", express.static(path.join(__dirname, "public")));
 
-console.log(process.env.EMAIL_USER);
-
 app.set("view engine", "ejs");
 
 // render home page
